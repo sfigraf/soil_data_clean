cdc9fdcb4f0b653f991e012f277a8c202ab7a580
sfigraf <sfigraf@gmail.com>
2022-04-11 15:21:37 -0600

    Initial: function cleans data, can upload and download data from excel sheet

---
+++ data_clean_app/app.R
@@ -0,0 +1,143 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+library(shinycssloaders)
+library(tidyverse)
+library(readxl)
+library(DT)
+#changes max upload to 600 mb
+options(shiny.maxRequestSize=600*1024^2)
+
+source("clean_sheet_function.R")
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    sidebarLayout(
+        sidebarPanel(
+            fileInput('file1', 'Insert File', accept = c(".xlsx")),
+            textInput('file1sheet','Name of Sheet (Case-Sensitive)'),
+        
+    ),#end of sidebar panel
+        mainPanel(tabsetPanel(
+            tabPanel("Uploaded Table",
+                     withSpinner(DT::dataTableOutput("table1"))),
+            tabPanel("Cleaned Data", 
+                     withSpinner(DT::dataTableOutput("table2")),
+                     hr(),
+                     downloadButton(outputId = "download1", label = "Save this data as CSV"),
+                     hr()),
+            tabPanel("Depth and Time Summaries", 
+                     withSpinner(DT::dataTableOutput("table3")),
+                     hr(),
+                     downloadButton(outputId = "download2", label = "Save this data as CSV"),
+                     hr())
+        ))#end of mainPanel 
+    ) #end of sidebarLayout
+    
+    
+    
+    #tableOutput("value")
+)
+
+server <- function(input, output) {
+    
+    sheets_name <- reactive({
+        if (!is.null(input$file1)) {
+            return(excel_sheets(path = input$file1$datapath))  
+        } else {
+            return(NULL)
+        }
+    })
+    
+    raw_data <- reactive({
+        if (!is.null(input$file1) && 
+            (input$file1sheet %in% sheets_name())) {
+            data <- read_excel(input$file1$datapath, 
+                               sheet = input$file1sheet,
+                               col_types = c("date", 
+                                             "text", "text", "text", "numeric", 
+                                             "numeric", "numeric", "numeric", 
+                                             "numeric", "numeric", "numeric", 
+                                             "numeric", "numeric", "numeric", 
+                                             "text", "text", "text", "text", "text", 
+                                             "text", "text", "text", "text", "text", 
+                                             "text", "text", "text", "text", "text", 
+                                             "text", "text", "text", "text", "text"), 
+                               
+                               skip = 1)
+            
+            return(data)
+        } else {
+            return(NULL)
+        }
+    })
+    
+    
+    output$table1 <- renderDT({
+        datatable(raw_data(), editable = TRUE)
+    })
+    
+    # proxy = dataTableProxy('table1')
+    # 
+    # observeEvent(input$x1_cell_edit, {
+    #     info = input$x1_cell_edit
+    #     str(info)
+    #     i = info$row
+    #     j = info$col
+    #     v = info$value
+    #     x[i, j] <<- DT::coerceValue(v, x[i, j])
+    #     replaceData(proxy, x, resetPaging = FALSE)  # important
+    # })
+    
+    cleaned_data <- reactive({
+        clean_sheet_function(raw_data(),input$file1sheet)
+    })
+
+    ##table 2 output
+    output$table2 <- renderDT({
+        datatable(cleaned_data()$clean_data)
+    })
+    
+    ##table 3 output
+    output$table3 <- renderDT({
+        datatable(cleaned_data()$depth_timestamp_summaries)
+    })
+    
+
+# Download Handlers -------------------------------------------------------
+
+    output$download1 <- downloadHandler(
+        filename = 
+            function() {
+                paste0("Cleaned_data_", str_replace_all(input$file1sheet, fixed(" "), ""), ".csv")
+            }
+        ,
+        content = function(file) {
+            write_csv(cleaned_data()$clean_data, file)
+            
+            
+        }
+    ) #end of download1
+    
+    output$download2 <- downloadHandler(
+        filename = 
+            function() {
+                paste0("Data_summaries_", str_replace_all(input$file1sheet, fixed(" "), ""),".csv")
+            }
+        ,
+        content = function(file) {
+            write_csv(cleaned_data()$depth_timestamp_summaries, file)
+            
+            
+        }
+    ) #end of download2    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ data_clean_app/data_cleaning_help.R
@@ -0,0 +1,88 @@
+library(tidyverse)
+library(lubridate)
+library(plotly)
+
+library(readxl)
+
+data <- read_excel("O2 temp and VWC data_all nodes_June21-Feb22.xlsx", 
+                                                          sheet = "Node 1", col_types = c("date", 
+                                                                                          "text", "text", "text", "numeric", 
+                                                                                          "numeric", "numeric", "numeric", 
+                                                                                          "numeric", "numeric", "numeric", 
+                                                                                          "numeric", "numeric", "numeric", 
+                                                                                          "text", "text", "text", "text", "text", 
+                                                                                          "text", "text", "text", "text", "text", 
+                                                                                          "text", "text", "text", "text", "text", 
+                                                                                          "text", "text", "text", "text", "text"), 
+                   
+                                                          skip = 1)
+data1 <- data[-c(1:2),c(1,5:14)]
+
+
+data2 <- data1 %>%
+  rename(O2_1_5cm = `O2_Avg(1)`,
+         O2_1_15cm = `O2_Avg(2)`,
+         O2_2_5cm = `O2_Avg(3)`,
+         O2_2_15cm = `O2_Avg(4)`,
+         O2_3_5cm = `O2_Avg(5)`,
+         O2_3_15cm = `O2_Avg(6)`,
+         O2_4_5cm = `O2_Avg(7)`,
+         O2_4_15cm = `O2_Avg(8)`,
+         O2_5_5cm = `O2_Avg(9)`,
+         O2_5_15cm = `O2_Avg(10)`) %>%
+  distinct()
+
+data3 <- data2 %>%
+  mutate(across(where(is.numeric), ~na_if(., "NaN")))
+
+data4 <- data3 %>%
+  gather(key = "sensor_number_and_depth", value = "value1", -TIMESTAMP)
+
+problem_rows <- data4 %>%
+  #filter(between(TIMESTAMP, as.Date("2021-06-01"), as.Date("2021-07-10")))
+  filter((TIMESTAMP >= as.Date("2021-06-01") & TIMESTAMP <= as.Date("2021-07-10")),
+         value1 <10)
+
+data6 <- anti_join(data4, problem_rows)
+#specific points to take out
+problem_points <- data6 %>%
+  #mutate(new_time1 = with_tz(TIMESTAMP, tzone = "US/Central")) %>%
+  filter(sensor_number_and_depth == "O2_2_15cm" & value1 <15)
+
+data7 <- anti_join(data6, problem_points)
+#in general only want oxygen values under 22
+data8 <- data7 %>%
+  filter(value1 < 22) %>%
+  mutate(depth = str_sub(sensor_number_and_depth, 6, -1),
+         node = "Node1")
+  
+
+data_summaries <- data8 %>%
+  group_by(depth, TIMESTAMP) %>%
+  #mutate(avg2 = mean(value1))
+  summarise(avg1 = mean(value1)) %>%
+  mutate(node = "Node1")
+
+
+
+
+
+# data4_1 <- data4 %>%
+#   #filter(between(TIMESTAMP, as.Date("2021-06-01"), as.Date("2021-07-10")))
+#   dplyr::filter((TIMESTAMP >= as.Date("2021-06-01")))
+
+ggplot(data8) +
+  aes(x = TIMESTAMP, y = value1) +
+  geom_point(shape = "circle", size = 1.5, colour = "#112446") +
+  theme_minimal() +
+  facet_wrap(vars(x))
+
+x <- data3 %>%
+  ggplot(aes(x = TIMESTAMP, y = O2_2_15cm)) +
+  geom_point()
+
+ggplotly(x)
+
+
+  #na_if(O2_1_5cm, "NaN")
+  #filter()
