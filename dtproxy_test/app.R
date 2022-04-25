93b0b3eea610d7d85cc5ec6b1cd8c8e564ed8e85
sfigraf <sfigraf@gmail.com>
2022-04-25 11:37:27 -0600

    proxy_text works! now to implement....

--- dtproxy_test/app.R
+++ dtproxy_test/app.R
@@ -3,17 +3,17 @@ library(tidyverse)
 library(DT)
 library(readxl)
 #df <- dplyr::tibble(Height = "185", Weight = "95")
-df <- read_excel("O2 temp and VWC data_all nodes_June21-Feb22.xlsx", 
-                 sheet = "Node 1", col_types = c("date", 
-                                                 "text", "text", "text", "numeric", 
-                                                 "numeric", "numeric", "numeric", 
-                                                 "numeric", "numeric", "numeric", 
-                                                 "numeric", "numeric", "numeric", 
-                                                 "text", "text", "text", "text", "text", 
-                                                 "text", "text", "text", "text", "text", 
-                                                 "text", "text", "text", "text", "text", 
-                                                 "text", "text", "text", "text", "text"), 
-                 
+df <- read_excel("O2 temp and VWC data_all nodes_June21-Feb22.xlsx",
+                 sheet = "Node 1", col_types = c("date",
+                                                 "text", "text", "text", "numeric",
+                                                 "numeric", "numeric", "numeric",
+                                                 "numeric", "numeric", "numeric",
+                                                 "numeric", "numeric", "numeric",
+                                                 "text", "text", "text", "text", "text",
+                                                 "text", "text", "text", "text", "text",
+                                                 "text", "text", "text", "text", "text",
+                                                 "text", "text", "text", "text", "text"),
+
                  skip = 1)
 source("oxygen_function.R")
 # df <- clean_sheet_function(df, "Node 1")
@@ -39,20 +39,20 @@ ui <- fluidPage(
             #                    choices = 1:nrow(df)),
 
             # shiny::actionButton(inputId = "remove", label = "Remove"),
-            # 
+            #
             # checkboxInput("checkbox1", "Take out Oxygen Range based on Dates?"),
             # hr(),
-            
+
              dateRangeInput("drange1",
                             "Date you want to take off",
                             start = "2021-06-01",
                             end = "2021-07-10"),
              sliderInput("slider2", "Oxygen range to take out of data between selected date range above",
                          min = 0,
-                         max = 50,  
+                         max = 50,
                          value = c(10,30),
                          step = 1),
-            
+
             actionButton("button1",
                          "Render/Update Data")
 
@@ -74,25 +74,25 @@ ui <- fluidPage(
 
 # server = function(input, output) {
 #     xx <- reactive({clean_sheet_function(df,input$nodename1)})
-#     
+#
 #     mod_df <- reactiveValues(x = xx)
-#     
+#
 #     #x$Date = Sys.time() + seq_len(nrow(x))
-#     
-#     
-#     
-# 
+#
+#
+#
+#
 #     observeEvent(input$button1, {
 #         #xx <- clean_sheet_function(df,input$nodename1)
-#         
-#         
-# 
+#
+#
+#
 #                 #problem with filter: slider1 becuase I didn't have a slider1 in the UI
 #                 # mod_df$x <- mod_df$x %>%
 #                 #     filter(
 #                 #         value1 >= input$slider1[1] & value1 <= input$slider1[2]
 #                 #     )
-# 
+#
 #                 problem_rows <- mod_df$x() %>%
 #                     #filter(between(TIMESTAMP, as.Date("2021-06-01"), as.Date("2021-07-10")))
 #                     filter((TIMESTAMP >= input$drange1[1] & TIMESTAMP <= input$drange1[2]),
@@ -101,7 +101,7 @@ ui <- fluidPage(
 #                 #this takes these rows out of the main dataframe
 #                 #
 #                 mod_df$x <- anti_join(mod_df$x(), problem_rows)
-# 
+#
 #                 # mod_df$x <- mod_df$x %>%
 #                 #     dplyr::bind_rows(
 #                 #         dplyr::tibble(Height = input$height,
@@ -109,20 +109,20 @@ ui <- fluidPage(
 #                 #     )
 #                 #DT::replaceData(proxy, mod_df$x)
 #                 #return(mod_df$x)
-# 
+#
 #             })
 #     proxy = dataTableProxy('table')
-#     
+#
 #     output$table = renderDT(
 #         isolate(mod_df$x() )
 #     )
-#     
+#
 #     shiny::observe({
-# 
+#
 #                 DT::replaceData(proxy, mod_df$x())
-# 
+#
 #             })
-# 
+#
 #     # Downloadable table (df) as csv
 #     output$downloadData = downloadHandler(
 #         filename = function() {
@@ -133,9 +133,9 @@ ui <- fluidPage(
 #         }
 #     )
 # }
-# 
-# 
-# 
+#
+#
+#
 # # Run the application
 # shinyApp(ui = ui, server = server)
 # Define server logic required to draw a histogram ----
@@ -144,26 +144,28 @@ server <- function(input, output, session) {
     #important to continuously remove values and prevent values from being added back to the table
 
     function_cleaned_data <- reactive({
-        clean_sheet_function(df,"Node 1")
+        clean_sheet_function(df,input$nodename1)
+        #can try putting filters inside this?
 
     })
-    mod_df <- reactiveValues(x = clean_sheet_function(df,"Node 1"))
 
-    #mod_df$x <- function_cleaned_data()
+    # need to set this to null at first; for some reason the reactiveValues() function doesn't work super well with reactives
+    # that's why mod_df$x is set/altered below in an observe context
+    mod_df <- reactiveValues(x = NULL) 
+    
+    # this observe has to be outside the filters below because we want each button press to decrease the amount of rows in the table; 
+    #otherwise, the function will just reset each time the button is pressed
+    observe({
+       mod_df$x <- function_cleaned_data()
+    })
+    
+    proxy <- DT::dataTableProxy('table')
+    
 
-
-    # shiny::observe({
-    #     shiny::updateSelectInput(session, inputId = "remove_row",
-    #                              choices = 1:nrow(mod_df$x))
-    # })
-
-    shiny::observeEvent(input$button1, {
+    observeEvent(input$button1, {
+        
 
         #problem with filter: slider1 becuase I didn't have a slider1 in the UI
-        # mod_df$x <- mod_df$x %>%
-        #     filter(
-        #         value1 >= input$slider1[1] & value1 <= input$slider1[2]
-        #     )
 
         problem_rows <- mod_df$x %>%
             #filter(between(TIMESTAMP, as.Date("2021-06-01"), as.Date("2021-07-10")))
@@ -171,32 +173,15 @@ server <- function(input, output, session) {
                    value1 >= input$slider2[1] & value1 <= input$slider2[2]
             )
         #this takes these rows out of the main dataframe
+        #x is not x() because
         mod_df$x <- anti_join(mod_df$x, problem_rows)
-
-        # mod_df$x <- mod_df$x %>%
-        #     dplyr::bind_rows(
-        #         dplyr::tibble(Height = input$height,
-        #                       Weight = input$weight)
-        #     )
         DT::replaceData(proxy, mod_df$x)
 
     })
 
-    # shiny::observeEvent(input$remove, {
-    #
-    #     mod_df$x <- mod_df$x[-as.integer(input$remove_row), ]
-    #
-    # })
-
-    proxy <- DT::dataTableProxy('table')
-    shiny::observe({
-
-        DT::replaceData(proxy, mod_df$x)
-
-    })
 
     output$table <- DT::renderDT({
-
+    #error: attempt to apply non-function once occurred because of using mod_df$x()
         mod_df$x
 
     })
@@ -218,6 +203,243 @@ server <- function(input, output, session) {
 
 }
 shinyApp(ui, server)
+
+# this was the code that got me to wrap my head around what worked!
+# df1 <- df_products_upload()
+# vals <- reactiveValues(
+#     df1 = NULL,
+#     data = NULL
+# )
+# 
+# # set vals$df1 whenever df1 updates
+# observe({
+#     vals$df1 <- df1()
+# })
+
+# library(shiny)
+# library(DT)
+# library(tidyverse)
+# 
+# # Define UI for application that edits the table
+# ui = fluidPage(
+#     DTOutput('x1'),
+#     
+#     # App title 
+#     titlePanel("Downloading Data"),
+#     
+#     # Sidebar layout with input and output definitions 
+#     sidebarLayout(
+#         
+#         # Sidebar panel for inputs 
+#         sidebarPanel(
+#             
+#             # Input: Choose dataset 
+#             selectInput("dataset", "Choose a dataset:",
+#                         choices = c("Demo Table")),
+#             
+#             # Button
+#             downloadButton("downloadData", "Download")
+#             
+#         ),
+#         
+#         # Main panel for displaying outputs 
+#         mainPanel(
+#             
+#             tableOutput("table")
+#             
+#         )
+#     ))
+# 
+# # Define server logic required
+# server = function(input, output) {
+#     x = iris
+#     x$Date = Sys.time() + seq_len(nrow(x))
+#     output$x1 = renderDT(x, selection = 'none', editable = TRUE)
+#     
+#     proxy = dataTableProxy('x1')
+#     
+#     observeEvent(input$x1_cell_edit, {
+#         info = input$x1_cell_edit
+#         str(info)
+#         i = info$row
+#         j = info$col
+#         v = info$value
+#         x[i, j] <<- DT::coerceValue(v, x[i, j])
+#         replaceData(proxy, x, resetPaging = FALSE)  # important
+#     })
+#     
+#     # Downloadable table (df) as csv
+#     output$downloadData = downloadHandler(
+#         filename = function() {
+#             paste(input$dataset, ".csv", sep = "")
+#         },
+#         content = function(file) {
+#             write.csv(x, file, row.names = FALSE)
+#         }
+#     )
+# }
+# 
+# 
+# 
+# # Run the application 
+# shinyApp(ui = ui, server = server)
+
+
+
+# Part that works ---------------------------------------------------------
+# 
+# library(shiny)
+# library(tidyverse)
+# library(DT)
+# library(readxl)
+# #df <- dplyr::tibble(Height = "185", Weight = "95")
+# df <- read_excel("O2 temp and VWC data_all nodes_June21-Feb22.xlsx", 
+#                  sheet = "Node 1", col_types = c("date", 
+#                                                  "text", "text", "text", "numeric", 
+#                                                  "numeric", "numeric", "numeric", 
+#                                                  "numeric", "numeric", "numeric", 
+#                                                  "numeric", "numeric", "numeric", 
+#                                                  "text", "text", "text", "text", "text", 
+#                                                  "text", "text", "text", "text", "text", 
+#                                                  "text", "text", "text", "text", "text", 
+#                                                  "text", "text", "text", "text", "text"), 
+#                  
+#                  skip = 1)
+# source("oxygen_function.R")
+# # df <- clean_sheet_function(df, "Node 1")
+# 
+# ui <- fluidPage(
+#     
+#     # App title ----
+#     titlePanel("DT + Proxy + Replace Data"),
+#     
+#     # Sidebar layout with input and output definitions ----
+#     sidebarLayout(
+#         
+#         # Sidebar panel for inputs ----
+#         sidebarPanel(
+#             
+#             # Input: Slider for the number of bins ----
+#             shiny::textInput(inputId = "height", label = "height"),
+#             shiny::textInput(inputId = "nodename1", label = "Nodename"),
+#             
+#             shiny::actionButton(inputId = "add", label = "Add"),
+#             
+#             
+#             
+#             dateRangeInput("drange1",
+#                            "Date you want to take off",
+#                            start = "2021-06-01",
+#                            end = "2021-07-10"),
+#             sliderInput("slider2", "Oxygen range to take out of data between selected date range above",
+#                         min = 0,
+#                         max = 50,  
+#                         value = c(10,30),
+#                         step = 1),
+#             
+#             actionButton("button1",
+#                          "Render/Update Data")
+#             
+#             
+#         ),
+#         
+#         # Main panel for displaying outputs ----
+#         mainPanel(
+#             
+#             # Output: Histogram ----
+#             DT::DTOutput(outputId = "table"),
+#             downloadButton(outputId = "soiltemp_download2", label = "Save this data as CSV")
+#             
+#             
+#         )
+#     )
+# )
+# 
+# 
+# 
+# # 
+# # # Run the application
+# # shinyApp(ui = ui, server = server)
+# # Define server logic required to draw a histogram ----
+# server <- function(input, output, session) {
+#     
+#     #important to continuously remove values and prevent values from being added back to the table
+#     
+#     function_cleaned_data <- reactive({
+#         clean_sheet_function(df,"Node 1")
+#         
+#     })
+#     mod_df <- reactiveValues(x = clean_sheet_function(df,"Node 1"))
+#     
+#     #mod_df$x <- function_cleaned_data()
+#     
+#     
+#     # shiny::observe({
+#     #     shiny::updateSelectInput(session, inputId = "remove_row",
+#     #                              choices = 1:nrow(mod_df$x))
+#     # })
+#     
+#     shiny::observeEvent(input$button1, {
+#         
+#         #problem with filter: slider1 becuase I didn't have a slider1 in the UI
+#         # mod_df$x <- mod_df$x %>%
+#         #     filter(
+#         #         value1 >= input$slider1[1] & value1 <= input$slider1[2]
+#         #     )
+#         
+#         problem_rows <- mod_df$x %>%
+#             #filter(between(TIMESTAMP, as.Date("2021-06-01"), as.Date("2021-07-10")))
+#             filter((TIMESTAMP >= input$drange1[1] & TIMESTAMP <= input$drange1[2]),
+#                    value1 >= input$slider2[1] & value1 <= input$slider2[2]
+#             )
+#         #this takes these rows out of the main dataframe
+#         mod_df$x <- anti_join(mod_df$x, problem_rows)
+#         
+#         # mod_df$x <- mod_df$x %>%
+#         #     dplyr::bind_rows(
+#         #         dplyr::tibble(Height = input$height,
+#         #                       Weight = input$weight)
+#         #     )
+#         DT::replaceData(proxy, mod_df$x)
+#         
+#     })
+#     
+#     # shiny::observeEvent(input$remove, {
+#     #
+#     #     mod_df$x <- mod_df$x[-as.integer(input$remove_row), ]
+#     #
+#     # })
+#     
+#     proxy <- DT::dataTableProxy('table')
+#     shiny::observe({
+#         
+#         DT::replaceData(proxy, mod_df$x)
+#         
+#     })
+#     
+#     output$table <- DT::renderDT({
+#         
+#         mod_df$x
+#         
+#     })
+#     
+#     
+#     output$soiltemp_download2 <- downloadHandler(
+#         filename =
+#             function() {
+#                 paste0("test.csv")
+#             }
+#         ,
+#         content = function(file) {
+#             write_csv(mod_df$x, file)
+#             
+#             
+#         }
+#     ) # end of soil temp downloads
+#     
+#     
+# }
+# shinyApp(ui, server)
 
 # library(shiny)
 # library(DT)
